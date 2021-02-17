7f611bef25143a8f18218dd7e43a9ea5a0f639d2
ZhiyuanYaoZhiyuanYao <zhiyuan.yao@sjsu.edu>
2021-02-16 23:41:43 -0700

    travel-review-service initial commit

---
+++ microservices/travel-review-service/src/test/java/com/travelassistantplatform/travelreview/TravelreviewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelassistantplatform.travelreview;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelreviewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
