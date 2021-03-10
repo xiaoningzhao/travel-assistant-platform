654b40bfc9d567508396a7b77eb24b18f9f65b25
Xiaoning Zhao <xiaoning.zhao@sjsu.edu>
2021-03-10 02:40:28 -0800

    Change logic of sending notification

--- microservices/notification-service/src/main/java/edu/sjsu/cmpe295/notificationservice/listeners/WebSocketSessionListener.java
+++ microservices/notification-service/src/main/java/edu/sjsu/cmpe295/notificationservice/listeners/WebSocketSessionListener.java
@@ -12,44 +12,44 @@ import org.springframework.web.socket.messaging.SessionSubscribeEvent;
 
 import java.util.List;
 
-@RequiredArgsConstructor
-@Component
+//@RequiredArgsConstructor
+//@Component
 public class WebSocketSessionListener {
-    private final NotificationService notificationService;
-
-    @EventListener
-    public void connectionEstablished(SessionConnectedEvent sce)
-    {
-        StompHeaderAccessor sha = StompHeaderAccessor.wrap((Message<?>) sce.getMessage().getHeaders().get("simpConnectMessage"));
-        List<String> nativeHeaders = sha.getNativeHeader("userId");
-        String sessionId = sha.getSessionId();
-        if( nativeHeaders != null )
-        {
-            String userId = nativeHeaders.get(0);
-            notificationService.addConnectedUser(userId, sessionId);
-        }
-
-    }
-
-    @EventListener
-    public void subscribeEstablished(SessionSubscribeEvent sce)
-    {
-        StompHeaderAccessor sha = StompHeaderAccessor.wrap(sce.getMessage());
-        String destination = sha.getNativeHeader("destination").get(0);
-        String topic = destination.split("/")[1];
-        if(topic.equals("user")) {
-            String userId = destination.split("/")[2];
-            notificationService.sendMissedNotificationToUser(userId);
-        }
-    }
-
-    @EventListener
-    public void webSocketDisconnect(SessionDisconnectEvent sde)
-    {
-        StompHeaderAccessor sha = StompHeaderAccessor.wrap(sde.getMessage());
-        String sessionId = sha.getSessionId();
-        notificationService.removeConnectedUser(sessionId);
-
-    }
+//    private final NotificationService notificationService;
+//
+//    @EventListener
+//    public void connectionEstablished(SessionConnectedEvent sce)
+//    {
+//        StompHeaderAccessor sha = StompHeaderAccessor.wrap((Message<?>) sce.getMessage().getHeaders().get("simpConnectMessage"));
+//        List<String> nativeHeaders = sha.getNativeHeader("userId");
+//        String sessionId = sha.getSessionId();
+//        if( nativeHeaders != null )
+//        {
+//            String userId = nativeHeaders.get(0);
+//            notificationService.addConnectedUser(userId, sessionId);
+//        }
+//
+//    }
+//
+//    @EventListener
+//    public void subscribeEstablished(SessionSubscribeEvent sce)
+//    {
+//        StompHeaderAccessor sha = StompHeaderAccessor.wrap(sce.getMessage());
+//        String destination = sha.getNativeHeader("destination").get(0);
+//        String topic = destination.split("/")[1];
+//        if(topic.equals("user")) {
+//            String userId = destination.split("/")[2];
+//            notificationService.sendMissedNotificationToUser(userId);
+//        }
+//    }
+//
+//    @EventListener
+//    public void webSocketDisconnect(SessionDisconnectEvent sde)
+//    {
+//        StompHeaderAccessor sha = StompHeaderAccessor.wrap(sde.getMessage());
+//        String sessionId = sha.getSessionId();
+//        notificationService.removeConnectedUser(sessionId);
+//
+//    }
 
 }
